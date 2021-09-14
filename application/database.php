909a2b502f1593c1ba6ceb1d5ee50b6307071c3a
zsddz <1715792133@qq.com>
2021-09-14 14:41:35 +0800

    composer

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.106.36.178',
     // 数据库名
     'database'        => 'thinkphp5',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'thinkphp5',
     // 密码
-    'password'        => 'root',
+    'password'        => 'iiTWwyxYZKn538tC',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
