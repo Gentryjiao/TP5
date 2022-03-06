490a54d3982a224bbef0de6fd1e4f94bec880835
张少东 <1715792133@qq.com>
2022-03-06 15:36:59 +0800

    table

---
+++ application/index/view/export.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>表格</title>
+	<style>
+        table {border-collapse:collapse;}
+        td,th{font-family:"宋体";font-size:12px;height: 30px;padding: 0 0 0 20px;}
+        th{font-size:18px;font-weight:normal;}
+        .td1{ width:50px;}
+        .td2{ width:200px;}
+        .td3{ width:100px;}
+        .td4{ width:140px;}
+        .td5{ width:130px;}
+        .td6{ width:160px;}
+        .num{ font-family:"Calibri";}
+        .a{padding:0 0 0 0;text-align: center}
+        .w794{width:794px;margin: 0px auto;}
+		.tdleft{text-align:left;width:50%;font-size:17px;}
+		.bold{font-weight:bold;}
+		td, th{padding:0px;font-size:15px;}
+		  @media print {
+			.noprint{
+			  display: none;
+			}
+		  }
+		  .PageNext{page-break-after: always;}
+	</style>
+    </head>
+  <body>
+	<div style="width:100%;margin:0px auto;">
+		<div class="w794 noprint">
+            <a href="javascript:window.print()" rel="external nofollow" target="_self">打印</a>
+		</div>
+		
+		<div id="print_content">
+
+        <div class="w794">
+			<table width="794px" cellpadding="0">
+				<tbody><tr class="lft">
+					<td class="tdleft bold">洽购商城</td>
+					<td class="tdleft bold">（团长配送清单）</td>
+				</tr>
+				<tr class="lft">
+					<td class=" ">团长名称：</td>
+					<td class=" ">团长电话：</td>
+				</tr>
+				<tr class="lft">
+					<td class=" ">配送地址：</td>
+					<td class=" "></td>
+				</tr>
+			</tbody></table>
+		</div>
+		<div class="w794">
+			<table width="792px" border="1" cellpadding="0">
+			  <tbody>
+              <tr class="a">
+				<td class="td1">序号</td>
+				<td class="td5">微信昵称</td>
+				<td class="td3">联系人</td>
+				<td class="td4">联系电话</td>
+				<td class="td2">商品与规格/数量</td>
+				<td class="td6">金额</td>
+				<td class="td6">签收</td>
+			  </tr>
+              <tr>
+                <td class="num" style="text-align:center;"></td>
+                <td style="text-align:center;">ㅤ防守打法</td>
+                <td style="text-align:center;"></td>
+                <td style="text-align:center;"></td>
+                <td style="text-align:center;"></td>
+                <td style="text-align:center;"></td>
+                <td></td>
+              </tr>
+
+			</tbody>
+            </table>
+		</div>
+		<div class="PageNext"></div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
